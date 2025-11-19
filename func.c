cd60e4a0ee9d05b44a74e6d9333a0b1b261ce45f
zaaanpact0 <l.gaek@yandex.ru>
2025-11-19 15:48:18 +0300

    2

--- func.c
+++ func.c
@@ -30,3 +30,30 @@ void erase_vector (Vector *v) {
     v->size = 0;
     v->capacity = 0;
 }
+
+int is_equal_vector(const Vector *v1, const Vector *v2) {
+    if (!v1 || !v2) return 0;
+    if (v1->size != v2->size) return 0;
+    
+    for (size_t i = 0; i < v1->size; i++) {
+        if (v1->CopyVoidPtr && v2->CopyVoidPtr) {
+            VECTOR_TYPE copy1 = v1->CopyVoidPtr(v1->data[i]);
+            VECTOR_TYPE copy2 = v2->CopyVoidPtr(v2->data[i]);
+            
+            if (copy1 != copy2) {
+                if (v1->DeleteVoidPtr) v1->DeleteVoidPtr(copy1);
+                if (v2->DeleteVoidPtr) v2->DeleteVoidPtr(copy2);
+                return 0;
+            }
+            
+            if (v1->DeleteVoidPtr) v1->DeleteVoidPtr(copy1);
+            if (v2->DeleteVoidPtr) v2->DeleteVoidPtr(copy2);
+        } else {
+            if (v1->data[i] != v2->data[i]) {
+                return 0;
+            }
+        }
+    }
+    
+    return 1;
+}
